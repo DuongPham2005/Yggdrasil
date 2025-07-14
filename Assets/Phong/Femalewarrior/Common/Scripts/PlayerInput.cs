97ec01ef3c6b1cf51c5f8d27c5f36815b3ff0ce0
Peter <manhphong02@gmail.com>
2025-07-14 23:01:59 +0700

    up goblin

--- Assets/Phong/Femalewarrior/Common/Scripts/PlayerInput.cs
+++ Assets/Phong/Femalewarrior/Common/Scripts/PlayerInput.cs
@@ -1,29 +1,49 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Retro.ThirdPersonCharacter
 {
     public class PlayerInput : MonoBehaviour
     {
-        private bool _attackInput;
-        private bool _specialAttackInput;
-        private Vector2 _movementInput;
-        private bool _jumpInput;
-        private bool _changeCameraModeInput;
+        private PlayerControls _controls;
 
-        public bool AttackInput {get => _attackInput;}
-        public bool SpecialAttackInput {get => _specialAttackInput;}
-        public Vector2 MovementInput {get => _movementInput;}
-        public bool JumpInput { get => _jumpInput; }
-        public bool ChangeCameraModeInput {get => _changeCameraModeInput;}
+        public bool AttackInput { get; private set; }
+        public bool SpecialAttackInput { get; private set; }
+        public Vector2 MovementInput { get; private set; }
+        public bool JumpInput { get; private set; }
+        public bool ChangeCameraModeInput { get; private set; }
+        public bool IsRunning { get; private set; }
 
-        private void Update()
+        private void Awake()
         {
-            _attackInput = Input.GetMouseButtonDown(0);
-            _specialAttackInput = Input.GetMouseButtonDown(1);
+            _controls = new PlayerControls();
 
-            _movementInput.Set(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
-            _jumpInput = Input.GetButton("Jump");
-            _changeCameraModeInput = Input.GetKeyDown(KeyCode.F);
+            _controls.Gameplay.Move.performed += ctx => MovementInput = ctx.ReadValue<Vector2>();
+            _controls.Gameplay.Move.canceled += ctx => MovementInput = Vector2.zero;
+
+            _controls.Gameplay.Jump.performed += ctx => JumpInput = true;
+            _controls.Gameplay.Jump.canceled += ctx => JumpInput = false;
+
+            _controls.Gameplay.Attack.performed += ctx => AttackInput = true;
+            _controls.Gameplay.Attack.canceled += ctx => AttackInput = false;
+
+            _controls.Gameplay.SpecialAttack.performed += ctx => SpecialAttackInput = true;
+            _controls.Gameplay.SpecialAttack.canceled += ctx => SpecialAttackInput = false;
+
+            _controls.Gameplay.ChangeCameraMode.performed += ctx => ChangeCameraModeInput = true;
+
+            _controls.Gameplay.Run.performed += ctx => IsRunning = true;
+            _controls.Gameplay.Run.canceled += ctx => IsRunning = false;
+        }
+
+        private void OnEnable() => _controls?.Enable();
+        private void OnDisable() => _controls?.Disable();
+
+        private void LateUpdate()
+        {
+            AttackInput = false;
+            SpecialAttackInput = false;
+            ChangeCameraModeInput = false;
         }
     }
 }
