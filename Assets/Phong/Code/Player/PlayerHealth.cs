1a87aec8a45a89aac33e64e3644e71561989cbf1
Darki-1 <haidarknguyen@gmail.com>
2025-07-18 14:33:55 +0700

    rain and Cloud
    done

--- Assets/Phong/Code/Player/PlayerHealth.cs
+++ Assets/Phong/Code/Player/PlayerHealth.cs
@@ -1,30 +1,30 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
-{
-    public int maxHealth = 100;
-    private int currentHealth;
+//public class PlayerHealth : MonoBehaviour
+//{
+//    public int maxHealth = 100;
+//    private int currentHealth;
 
-    void Start()
-    {
-        currentHealth = maxHealth;
-    }
+//    void Start()
+//    {
+//        currentHealth = maxHealth;
+//    }
 
-    public void TakeDamage(int amount)
-    {
-        currentHealth -= amount;
-        Debug.Log("Player bị đánh! Máu còn: " + currentHealth);
+//    public void TakeDamage(int amount)
+//    {
+//        currentHealth -= amount;
+//        Debug.Log("Player bị đánh! Máu còn: " + currentHealth);
 
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-    }
+//        if (currentHealth <= 0)
+//        {
+//            Die();
+//        }
+//    }
 
-    void Die()
-    {
-        // Tắt điều khiển, chơi animation chết...
-        Debug.Log("Player chết");
-    }
-}
+//    void Die()
+//    {
+//        // Tắt điều khiển, chơi animation chết...
+//        Debug.Log("Player chết");
+//    }
+//}
 
