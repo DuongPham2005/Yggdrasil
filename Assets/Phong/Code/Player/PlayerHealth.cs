70bff712555ae5942090ad560c37a0dac33abf24
Peter <manhphong02@gmail.com>
2025-07-23 18:38:49 +0700

    up player

--- Assets/Phong/Code/Player/PlayerHealth.cs
+++ Assets/Phong/Code/Player/PlayerHealth.cs
@@ -1,53 +1,53 @@
-using Retro.ThirdPersonCharacter;
-using UnityEngine;
+//using Retro.ThirdPersonCharacter;
+//using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
-{
-    public int maxHealth = 100;
-    private int currentHealth;
+//public class PlayerHealth : MonoBehaviour
+//{
+//    public int maxHealth = 100;
+//    private int currentHealth;
 
-    private Animator animator;
+//    private Animator animator;
 
-    void Start()
-    {
-        currentHealth = maxHealth;
-        animator = GetComponent<Animator>();
-    }
+//    void Start()
+//    {
+//        currentHealth = maxHealth;
+//        animator = GetComponent<Animator>();
+//    }
 
-    public void TakeDamage(int amount)
-    {
-        if (currentHealth <= 0) return; // Đã chết thì không xử lý nữa
+//    public void TakeDamage(int amount)
+//    {
+//        if (currentHealth <= 0) return; // Đã chết thì không xử lý nữa
 
-        currentHealth -= amount;
-        Debug.Log("Player bị đánh! Máu còn: " + currentHealth);
+//        currentHealth -= amount;
+//        Debug.Log("Player bị đánh! Máu còn: " + currentHealth);
 
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-        else
-        {
-            animator.SetBool("IsHit", true); // Bật animation trúng đòn
-        }
-    }
+//        if (currentHealth <= 0)
+//        {
+//            Die();
+//        }
+//        else
+//        {
+//            animator.SetBool("IsHit", true); // Bật animation trúng đòn
+//        }
+//    }
 
-    void Die()
-    {
-        Debug.Log("Player chết");
+//    void Die()
+//    {
+//        Debug.Log("Player chết");
 
-        animator.SetBool("Die", true); // Chạy animation chết
+//        animator.SetBool("Die", true); // Chạy animation chết
 
-        // Vô hiệu hóa điều khiển
-        var move = GetComponent<Movement>();
-        if (move != null) move.enabled = false;
+//        // Vô hiệu hóa điều khiển
+//        var move = GetComponent<Movement>();
+//        if (move != null) move.enabled = false;
 
-        var combat = GetComponent<Combat>();
-        if (combat != null) combat.enabled = false;
-    }
+//        var combat = GetComponent<Combat>();
+//        if (combat != null) combat.enabled = false;
+//    }
 
-    // Gọi từ animation event cuối clip trúng đòn
-    public void EndHitReact()
-    {
-        animator.SetBool("IsHit", false);
-    }
-}
+//    // Gọi từ animation event cuối clip trúng đòn
+//    public void EndHitReact()
+//    {
+//        animator.SetBool("IsHit", false);
+//    }
+//}
